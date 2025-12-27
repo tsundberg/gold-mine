cc89b71c28d1aecf5f8b963e1f3d644599247a6f
thomassundberg <thomas@thinkcode.se>
2025-12-27 20:12:51 +0200

    Introduced a star explorer for exploring the map

---
+++ src/main/java/se/thinkcode/gold/mine/explorer/a/star/AStarExplorer.java
@@ -0,0 +1,17 @@
+package se.thinkcode.gold.mine.explorer.a.star;
+
+import se.thinkcode.gold.mine.explorer.Explorer;
+import se.thinkcode.gold.mine.explorer.GoldMineExplorer;
+
+public class AStarExplorer implements Explorer {
+    public AStarExplorer(GoldMineExplorer goldMineExplorer) {
+        // todo implement me
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Override
+    public void explore() {
+        // todo implement me
+        throw new RuntimeException("Not yet implemented");
+    }
+}
